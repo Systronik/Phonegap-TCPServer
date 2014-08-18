a49b6d6fa748a2770f13ed0c82421442dce815da
Systronik <jaeggle@systronik.de>
2014-08-18 14:05:51 +0200

    New structure

--- www/TCPServer.js
+++ www/TCPServer.js
@@ -1,9 +1,7 @@
-cordova.define("org.systronik.tcpserver.TCPServer", function(require, exports, module) {
 var tcpserver = {
     startServer: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "TCPServer", "startServer", []);
     }
-};
-module.exports = tcpserver;
-});
+}
+module.exports = calendar;
 
