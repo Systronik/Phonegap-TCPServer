2406304ffc4129a6570f2c119bb2455a097acba1
Systronik <jaeggle@systronik.de>
2014-08-18 14:09:39 +0200

    js file removed to rename later

--- www/TCPServer.js
+++
@@ -1,7 +0,0 @@
-var tcpserver = {
-    startServer: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "TCPServer", "startServer", []);
-    }
-}
-module.exports = calendar;
-
