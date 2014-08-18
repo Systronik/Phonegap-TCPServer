203b74f5377c151c988e6e4bac3a87fa4983eeec
Systronik <jaeggle@systronik.de>
2014-08-18 16:02:34 +0200

    js file updated

--- www/tcpserver.js
+++ www/tcpserver.js
@@ -1,12 +1,10 @@
-cordova.define("org.systronik.tcpserver.TCPServer", function(require, exports, module) { var tcpserver = {
+var tcpserver = {
     startServer: function(dataToSend, port, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "TCPServer", "startServer", [{
         	"dataToSend": dataToSend,
         	"port": port
         }]);
     }
-}
+};
 module.exports = tcpserver;
 
-
-});
