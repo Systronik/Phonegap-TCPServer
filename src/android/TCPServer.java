d978e8a883deca882e1d447903cfb7a3ea423231
Systronik <jaeggle@systronik.de>
2014-08-19 15:38:14 +0200

    Java file updated
    Server now accepts port settings, converts TransmissionId and accepts
    non GET commands

--- src/android/TCPServer.java
+++ src/android/TCPServer.java
@@ -20,7 +20,6 @@ public class TCPServer extends CordovaPlugin {
 
   private static final String TAG = "TCPServer";
   private static final String ACTION_START_SERVER = "startServer";
-  //private String getExample = "HTTP/1.1 200 OK\r\nDate: Mon, 23 May 2005 22:38:34 GMT\r\nServer: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\nLast-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\nETag: \"3f80f-1b6-3e1cb03b\"\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 124\r\nAccept-Ranges: bytes\r\nConnection: close\r\n\r\n<html>\r\n<head>\r\n<title>An Example Page</title>\r\n</head>\n<body>\r\nHello World, this is a very simple 1234 document.\r\n</body>\r\n</html>\r\n";
   private String OutString;
 
   ServerSocket myServerSocket;
@@ -33,6 +32,7 @@ public class TCPServer extends CordovaPlugin {
       if (ACTION_START_SERVER.equals(action)) {
     	  JSONObject arg_object = args.getJSONObject(0);
     	  OutString = arg_object.getString("dataToSend");
+    	  final int port =  Integer.parseInt(arg_object.getString("port"));
     	  if (!(OutString.contains("HTTP/1.1"))){
     		  //OutString = getExample;
     	  }
@@ -43,7 +43,7 @@ public class TCPServer extends CordovaPlugin {
     		    	Log.d(TAG, "Server is starting...");    	  
     				  try
     			        { 
-    			            myServerSocket = new ServerSocket(53000); 
+    			            myServerSocket = new ServerSocket(port); 
     			            Log.d(TAG, "server started");
     			            ServerActivated = true;
     			            
@@ -140,31 +140,22 @@ public class TCPServer extends CordovaPlugin {
               //out = new PrintWriter(new OutputStreamWriter(myClientSocket.getOutputStream())); 
               OutputStream output = myClientSocket.getOutputStream();
               // At this point, we can read for input and reply with appropriate output. 
-              
               // Run in a loop until m_bRunThread is set to false 
               while(m_bRunThread) 
               {                    
                   // read incoming stream 
-                  String clientCommand = in.readLine(); 
+                  String clientCommand = in.readLine();
+                  String sendString;
                   if (clientCommand != null){
                 	  Log.d(TAG, "Client Says :" + clientCommand);                 	  
                   }
                   
-
                   if(!ServerOn) 
                   { 
                       // Special command. Quit this thread 
-                	  Log.d(TAG, "Server has already stopped");                       
-                      //out.println("Server has already stopped"); 
-                      //out.flush(); 
+                	  Log.d(TAG, "Server has already stopped");                                          
                       m_bRunThread = false;   
-
                   } 
-                  
-                  
-                  //long time = System.currentTimeMillis();
-                  
-					
                   if(clientCommand.equalsIgnoreCase("quit")) { 
                       // Special command. Quit this thread 
                       m_bRunThread = false;   
@@ -174,16 +165,45 @@ public class TCPServer extends CordovaPlugin {
                       m_bRunThread = false;  
                       Log.d(TAG, "Stopping client thread for client : ");          
                       ServerOn = false;
-                  } else if(clientCommand.contains("GET")) { 
+                  } else if(clientCommand.contains("GET")) {										// Data with html header   
                       // Get Command
-                	  output.write(OutString.getBytes());
+                	  Log.d(TAG, "GET - Command");
+                	  sendString = OutString;
+                	  int i = clientCommand.indexOf("id=");			// Replaces TransmissionId
+                	  if (i > -1){
+                		  String idString = clientCommand.substring(i + 3);
+                		  i = idString.indexOf("http/");
+                		  if ( i == -1){
+                			  i = idString.indexOf("HTTP/");
+                		  }
+                		  if ( i > -1){
+                			  idString = idString.substring(0, i);
+                			  idString = idString.trim();
+                			  Log.d(TAG, "ID found: " + idString);
+                			  sendString = sendString.replace("TransmissionId=\"0\"", "TransmisionId=\"" + idString + "\"");
+                			  idString = sendString.substring(sendString.indexOf("\r\n\r\n") + 2);               			  
+                			  String str = "HTTP/1.1 200 OK\r\n" +
+                						"Server: Apache/1.3.29 (Unix) PHP/4.3.4\r\n" +
+                						"Content-Length: " + idString.length() + "\r\n" +
+                						"Content-Language: de\r\n" +
+                						"Connection: close\r\n" +
+                						"Access-Control-Allow-Origin: *\r\n" +
+                						"Content-Type: text/html\r\n\r\n" + 
+                						idString;
+                			  sendString = str;
+                		  }
+                		  
+                	  }
+                	  output.write(sendString.getBytes());
                 	  output.flush();
-                  } else {
-                          // Process it
-                	  	  Log.d(TAG, "Server sending...");                	  	  
-                          //out.println("Server Says : " + clientCommand); 
-                          //out.print(&OutString[0]);
-                          //out.flush(); 
+                	  Log.d(TAG, "Data written");
+                  } else {																			// Data without html header  
+                	  sendString = OutString.substring(OutString.indexOf("\r\n\r\n") + 2);   
+            	  	  output.write(sendString.getBytes());
+            	  	  output.flush();
+                      //out.println("Server Says : " + clientCommand); 
+                      //out.print(&OutString[0]);
+                      //out.flush(); 
                   }
               } 
           } 
